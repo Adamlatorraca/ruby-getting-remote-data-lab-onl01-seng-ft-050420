ebeb3e490c2a8f2ee5b2745cf015d85207c77e35
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-20 22:52:21 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -6,7 +6,7 @@ require 'json'
 class GetRequester
   URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
 
-  def initialize(URL)
-    @URL = URL
+  def initialize(url)
+    @url = url
   end
 end
